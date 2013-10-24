21686f45f987ff3b486f2af8787d338daf8a3427
Nikolai Plath <der.el.kuku@gmail.com>
2013-10-24 12:45:42 -0500

    Move the site to /home/vagrant/server/htdocs Update requirements

--- build/puppet/manifests/default.pp
+++ build/puppet/manifests/default.pp
@@ -1,5 +1,5 @@
 Exec {
-path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
+	path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
 }
 
 exec { 'apt-get update':
@@ -9,33 +9,43 @@ exec { 'apt-get update':
 #include joomla-release
 
 class joomla-release {
-$dir = '/var/www/joomla-cms'
-$path = 'http://joomlacode.org/gf/download/frsrelease/18622/83486'
-$file = 'Joomla_3.1.5-Stable-Full_Package.tar.gz'
-exec { 'setup':
-	creates => "${dir}",
-	command => "mkdir ${dir} && cd ${dir} && wget ${path}/${file} && tar xzf ${file} && rm ${file} && chmod 0777 -R ${dir}",
-	require => Package['php5', 'apache2'],
-	notify  => Service['apache2'],
-}
+	$dir = '/home/vagrant/server/htdocs/joomla-cms'
+	$path = 'http://joomlacode.org/gf/download/frsrelease/18622/83486'
+	$file = 'Joomla_3.1.5-Stable-Full_Package.tar.gz'
+	exec { 'setup':
+		creates => "${dir}",
+		command => "mkdir ${dir} && cd ${dir} && wget ${path}/${file} && tar xzf ${file} && rm ${file} && chmod 0777 -R ${dir}",
+		require => Package['php5', 'apache2'],
+		notify  => Service['apache2'],
+	}
 }
 
 include joomla-git
 
 class joomla-git {
+	$dir = '/home/vagrant/server'
+	$repo = 'https://github.com/joomla/joomla-cms.git'
 	package { ['git-core']:
 		ensure => installed,
 		require => Exec['apt-get update'],
 	}
-$dir = '/var/www/joomla-cms'
-$repo = 'https://github.com/joomla/joomla-cms.git'
-exec { 'setup':
-	creates => "${dir}",
-	command => "echo 'cloning...' && cd /var/www && git clone ${repo} && chmod 0777 -R ${dir}",
-	require => Package['php5', 'apache2', 'git-core'],
-	notify  => Service['apache2'],
-	timeout => 0,
-}
+
+	file {
+		[
+			"/home/vagrant/server",
+			"/home/vagrant/server/htdocs",
+			"/home/vagrant/server/logs"
+		]:
+		ensure => "directory",
+	}
+
+	exec { 'setup':
+		creates => "${dir}/htdocs/joomla-cms",
+		command => "echo 'cloning...' && cd ${dir}/htdocs && git clone ${repo} && chown -R vagrant:vagrant ${dir}",
+		require => Package['php5', 'apache2', 'git-core'],
+		notify  => Service['apache2'],
+		timeout => 0,
+	}
 }
 
 include system
@@ -61,10 +71,10 @@ class apache {
 		require => Package['apache2'],
 	}
 
-	exec { 'enable-mod_rewrite':
+	exec { 'enable-required-modules':
 		require => Package['apache2'],
 		before => Service['apache2'],
-		command => 'a2enmod rewrite'
+		command => 'a2enmod rewrite php5'
 	}
 
 	file { 'joomla-cms-site':
@@ -81,6 +91,14 @@ class apache {
 		],
 		before => Service['apache2'],
 		command => 'a2dissite 000-default && a2ensite site-joomla-cms'
+	}
+
+	exec { 'change-apache-user':
+		require => [
+			Package['apache2']
+		],
+		before => Service['apache2'],
+		command => "sed -i 's/www-data/vagrant/g' /etc/apache2/envvars && chown vagrant:vagrant /var/lock/apache2"
 	}
 }
 
@@ -103,31 +121,37 @@ include php
 
 class php {
 	exec { 'modify-repos-for-php55':
-		command => 'cat /vagrant/build/puppet/files/dotdeb-php55-sources.list >> /etc/apt/sources.list && wget http://www.dotdeb.org/dotdeb.gpg && cat dotdeb.gpg | apt-key add - && apt-get update',
+		command => 'cat /vagrant/build/puppet/files/dotdeb-php55-sources.list >> /etc/apt/sources.list && wget http://www.dotdeb.org/dotdeb.gpg && cat dotdeb.gpg | apt-key add - && apt-get update && rm dotdeb.gpg',
 	}
 
 	package { [
 		'php5',
-		'php5-mysql',
+		'php5-cli',
 		'php5-curl',
-		'php5-cli', 'php5-xdebug'
+		'php5-mysql',
+		'php5-xdebug'
 	]:
 		ensure  => 'installed',
 		require => [
 			Package['apache2'],
-			Exec['apt-get update', 'modify-repos-for-php55']
+			Exec[
+				'apt-get update',
+				'modify-repos-for-php55'
+			]
 		],
 	}
 
-	file { '/etc/php5/apache2filter/conf.d/21-xdebug.ini':
-#	file { '/etc/php5/apache2/conf.d/21-xdebug.ini':
+#	file { '/etc/php5/apache2filter/conf.d/21-xdebug.ini':
+	file { '/etc/php5/apache2/conf.d/21-xdebug.ini':
+#	file { '/etc/php5/conf.d/21-xdebug.ini':
 		ensure => present,
 		require => Package['php5'],
 		source => '/vagrant/build/puppet/files/21-xdebug.ini';
 	}
 
-	file { '/etc/php5/apache2filter/conf.d/666-php.ini':
-#	file { '/etc/php5/apache2/conf.d/666-php.ini':
+#	file { '/etc/php5/apache2filter/conf.d/666-php.ini':
+	file { '/etc/php5/apache2/conf.d/666-php.ini':
+#	file { '/etc/php5/conf.d/666-php.ini':
 		ensure => present,
 		require => Package['php5'],
 		source => '/vagrant/build/puppet/files/666-php.ini';
