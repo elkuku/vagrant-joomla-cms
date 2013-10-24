254ec53d35181d715834c8faf8d2ec09af3bdb86
Nikolai Plath <der.el.kuku@gmail.com>
2013-10-24 13:31:26 -0500

    Move the site to /home/vagrant/server/htdocs Update requirements

--- build/puppet/manifests/default.pp
+++ build/puppet/manifests/default.pp
@@ -1,5 +1,5 @@
 Exec {
-path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
+	path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
 }
 
 exec { 'apt-get update':
@@ -9,15 +9,25 @@ exec { 'apt-get update':
 include joomla
 
 class joomla {
-$dir = '/var/www/joomla-cms'
-$path = 'http://joomlacode.org/gf/download/frsrelease/18622/83486'
-$file = 'Joomla_3.1.5-Stable-Full_Package.tar.gz'
-exec { 'setup':
-	creates => "${dir}",
-	command => "mkdir ${dir} && cd ${dir} && wget ${path}/${file} && tar xzf ${file} && rm ${file} && chmod 0777 -R ${dir}",
-	require => Package['php5', 'apache2'],
-	notify  => Service['apache2'],
-}
+	$dir = '/home/vagrant/server/htdocs/joomla-cms'
+	$path = 'http://joomlacode.org/gf/download/frsrelease/18622/83486'
+	$file = 'Joomla_3.1.5-Stable-Full_Package.tar.gz'
+	file {
+		[
+		"/home/vagrant/server",
+		"/home/vagrant/server/htdocs",
+		"/home/vagrant/server/logs"
+		]:
+		ensure => "directory",
+	}
+
+	exec { 'setup':
+		creates => "${dir}",
+		command => "mkdir ${dir} && cd ${dir} && wget ${path}/${file} && tar xzf ${file} && chown -R vagrant:vagrant ${dir} && rm ${file}",
+		require => Package['php5', 'apache2'],
+		notify  => Service['apache2'],
+		timeout => 0,
+	}
 }
 
 include system
@@ -43,17 +53,34 @@ class apache {
 		require => Package['apache2'],
 	}
 
-	exec { 'enable-mod_rewrite':
+	exec { 'enable-required-modules':
 		require => Package['apache2'],
 		before => Service['apache2'],
-		command => '/usr/sbin/a2enmod rewrite'
+		command => 'a2enmod rewrite'
 	}
 
-	# OK, this is ugly... wtf
-	exec { 'copy-sites-enabled':
+	file { 'joomla-cms-site':
+		ensure => present,
 		require => Package['apache2'],
+		path => '/etc/apache2/sites-available/site-joomla-cms',
+		source => '/vagrant/build/puppet/files/site-joomla-cms';
+	}
+
+	exec { 'update-sites-available':
+		require => [
+			Package['apache2'],
+			File['joomla-cms-site']
+		],
 		before => Service['apache2'],
-		command => 'rm /etc/apache2/sites-enabled/000-default && cp /vagrant/build/puppet/files/000-default /etc/apache2/sites-enabled/'
+		command => 'a2dissite 000-default && a2ensite site-joomla-cms'
+	}
+
+	exec { 'change-apache-user':
+		require => [
+		Package['apache2']
+		],
+		before => Service['apache2'],
+		command => "sed -i 's/www-data/vagrant/g' /etc/apache2/envvars && chown vagrant:vagrant /var/lock/apache2"
 	}
 }
 
@@ -77,10 +104,15 @@ include php
 class php {
 	package { [
 		'php5',
+		'php5-cli',
+		'php5-curl',
 		'php5-mysql',
-		'php5-curl'
+		'php5-xdebug'
 	]:
 		ensure  => 'installed',
-		require => Exec['apt-get update'],
+		require => [
+			Package['apache2'],
+			Exec['apt-get update']
+		],
 	}
 }
